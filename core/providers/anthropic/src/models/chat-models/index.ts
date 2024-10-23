a9d7a7e7b9cd47ee5d53fb97a17850f2b35febad
Akshay Gulati <akshay@adaline.ai>
2024-10-22 23:24:03 -0700

    chore: add anthropic claude-3.5-sonnet-20241022

--- core/providers/anthropic/src/models/chat-models/index.ts
+++ core/providers/anthropic/src/models/chat-models/index.ts
@@ -4,3 +4,4 @@ export * from "./claude-3-haiku-20240307.anthropic";
 export * from "./claude-3-sonnet-20240229.anthropic";
 export * from "./claude-3-opus-20240229.anthropic";
 export * from "./claude-3-5-sonnet-20240620.anthropic";
+export * from "./claude-3-5-sonnet-20241022.anthropic";
