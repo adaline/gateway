fc406ed9a9ca643bf7d414a982cde356d77f80df
AyushR1 <ayushrathore12501@gmail.com>
2025-01-13 19:59:29 +0530

    chore: add support for more anthropic models
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

---
+++ core/providers/anthropic/src/models/chat-models/claude-3-5-sonnet-latest.anthropic.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+
+import { ChatModelSchema } from "@adaline/provider";
+
+import { AnthropicChatModelConfigs } from "../../configs";
+import { BaseChatModel, BaseChatModelOptions } from "./base-chat-model.anthropic";
+import {
+  AnthropicChatModelModalities,
+  AnthropicChatModelModalitiesEnum,
+  AnthropicChatModelRoles,
+  AnthropicChatModelRolesMap,
+} from "./types";
+
+const Claude3_5SonnetLatestLiteral = "claude-3-5-sonnet-latest";
+const Claude3_5SonnetLatestDescription = "Most intelligent model. Highest level of intelligence and capability.";
+
+const Claude3_5SonnetLatestSchema = ChatModelSchema(AnthropicChatModelRoles, AnthropicChatModelModalitiesEnum).parse({
+  name: Claude3_5SonnetLatestLiteral,
+  description: Claude3_5SonnetLatestDescription,
+  maxInputTokens: 200000,
+  maxOutputTokens: 8192,
+  roles: AnthropicChatModelRolesMap,
+  modalities: AnthropicChatModelModalities,
+  config: {
+    def: AnthropicChatModelConfigs.base(8192, 4).def,
+    schema: AnthropicChatModelConfigs.base(8192, 4).schema,
+  },
+});
+
+const Claude3_5SonnetLatestOptions = BaseChatModelOptions;
+type Claude3_5SonnetLatestOptionsType = z.infer<typeof Claude3_5SonnetLatestOptions>;
+
+class Claude3_5SonnetLatest extends BaseChatModel {
+  constructor(options: Claude3_5SonnetLatestOptionsType) {
+    super(Claude3_5SonnetLatestSchema, options);
+  }
+}
+
+export {
+  Claude3_5SonnetLatest,
+  Claude3_5SonnetLatestLiteral,
+  Claude3_5SonnetLatestOptions,
+  Claude3_5SonnetLatestSchema,
+  type Claude3_5SonnetLatestOptionsType,
+};
